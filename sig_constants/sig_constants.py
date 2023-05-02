5b00eb10e5458ae560760fd476d36f6f78ee5f19
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-05-02 01:50:39 -0700

    Initial commit

---
+++ sig_constants/sig_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+https://github.com/steph1111/PERCISE/blob/main/sig_float/sig_float.py
+Module sig_constant contains the sig_constant class--common mathatical
+constants of type sig_float
+"""
+__author__ = "Stephanie L'Heureux"
+
+# import sys 
+# sys.path.append("/Users/stephanie/Documents/PERCISE/sig_float/")
+# import sig_float
+from PERCISE.sig_float import sig_float
+
+# class sig_constants:
+  
