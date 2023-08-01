ca51ebcf6556f351d6ca1de61dd617b4eb12e853
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-08-01 01:35:30 -0700

    FINALLY work on issue #26

--- sig_constants/sig_constants.py
+++ sig_constants/sig_constants.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 """
 https://github.com/steph1111/PERCISE/blob/main/sig_float/sig_float.py
-Module sig_constant contains the sig_constant class--common mathatical
+Module sig_constant contains the sig_constant class--common mathematical
 constants of type sig_float
 """
 __author__ = "Stephanie L'Heureux"
 
 import sys 
-# sys.path.append("/Users/stephanie/Documents/PERCISE/sig_float")
 sys.path.insert(0, "/Users/stephanie/Documents/PERCISE/sig_float") #allows import of modules from non-system folder
-import sig_float
+from sig_float import sig_float
 
 
 class sig_constants:
-  PI = sig_float("3.14159265358979323846264")
-  e = sig_float("2.71828182845904523536")
-  C = sig_float("299792458", {"m":1}, {"s":1})
-  # h = ("6.62607015 x 10^", ) PLANKS CONSTANT, need scientific notation
-  # AVOGADROS_NUMBER = (" 6.02214076 × 10^23")
+  PI = sig_float("3.141592653589793238462643383279502884197", exact=True)
+  e = sig_float("2.71828182845904523536028747135266249", exact=True)
+  C = sig_float("299792458", {"m":1, "s":1}, exact=True)
+  h = sig_float("6.62607015e-34", {"J":1, "s":1}, exact=True)
+  NA = sig_float(" 6.02214076e+23", exact=True)
   R_atm = sig_float("0.08206", {"L":1, "atm":1}, {"K":1, "mol":1})
   R_mmHg = sig_float("62.36", {"L":1, "mmHg":1}, {"K":1, "mol":1})
-
-  
