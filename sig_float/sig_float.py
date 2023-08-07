cd82df0cd6db98593f34b49c6f99247ef856e045
Stephanie L'Heureux <stephanielh1111@gmail.com>
2023-08-06 23:27:34 -0700

    Issue #46

--- sig_float/sig_float.py
+++ sig_float/sig_float.py
@@ -92,6 +92,9 @@ def round_sig(number, sig_figs: int):  #->sig_float
   return rounded_sig_float
 
 
+########################################
+#           SIG FLOAT CLASS            #
+########################################
 class sig_float:
   """
   Defines a numerical float-like type that obides to significant figure rules:
@@ -643,5 +646,17 @@ class sig_float:
       self = sig_float(other)
 
 
-if __name__ == "__main__":
-  pass
+########################################
+#           SIG CONST CLASS            #
+########################################
+class sig_const:
+  """
+  Defines several common constants of type sig_float
+  """
+  PI = sig_float("3.141592653589793238462643383279502884197", exact=True)  # Pi
+  e = sig_float("2.71828182845904523536028747135266249", exact=True)  # Euler's number
+  C = sig_float("299792458", {"m":1, "s":1}, exact=True)  # Speed of light
+  h = sig_float("6.626e-34", {"J":1, "s":1}, exact=True)  # Planck's constant 
+  NA = sig_float(" 6.022+23", exact=True)  # Avogadro's number
+  R_atm = sig_float("0.08206", {"L":1, "atm":1}, {"K":1, "mol":1})  # Gas constant (atm)
+  R_mmHg = sig_float("62.36", {"L":1, "mmHg":1}, {"K":1, "mol":1})  # Gas constant (mmHg)
